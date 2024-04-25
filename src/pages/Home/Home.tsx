aeb37b7d83a4bfc04d7412f7b6f018678d0dc82c
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-04-25 09:23:52 +0900

    add code to track user position

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import MapboxMap, { NavigationControl } from "react-map-gl";
 
-import { getCurrentLocation } from "@/lib/utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBicycle,
@@ -9,67 +8,52 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { useMap } from "react-map-gl";
 import { MapMarker } from "./MapMarker";
+import { getCurrentLocation } from "@/lib/utils";
 
 export default function Home() {
   const [initialState, setInitialState] = useState({});
   const [permissionGranted, setPermissionGranted] = useState(false);
   const [showUserLocationMarker, setShowUserLocationMarker] = useState(false);
-  const [userCurrentPosition, setUserCurrentPosition] = useState<{
-    latitude: number;
-    longitude: number;
-  }>({ latitude: 0, longitude: 0 });
+  const [userCurrentPosition, setUserCurrentPosition] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
   const [updatedTimes, setUpdatedTimes] = useState(0);
+
   useEffect(() => {
     loadInitialState();
   }, []);
-
-  useEffect(() => {
-    let intervalId: NodeJS.Timeout;
-
-    if (showUserLocationMarker) {
-      intervalId = setInterval(() => {
-        setUserCurrentPosition((prevPosition) => ({
-          latitude: prevPosition.latitude + 0.0005,
-          longitude: prevPosition.longitude,
-        }));
-      }, 500);
-    }
-
-    return () => clearInterval(intervalId);
-  }, [showUserLocationMarker]);
   const { mainMap } = useMap();
-  async function flyToUserCurrentPosition() {
-    try {
-      if (!showUserLocationMarker) {
-        const { latitude, longitude } = await getCurrentLocation();
-        mainMap?.flyTo({ center: [longitude, latitude] });
-        setUserCurrentPosition({ latitude, longitude });
-        setShowUserLocationMarker(true);
-        return;
-      }
-      mainMap?.flyTo({
-        center: [userCurrentPosition.longitude, userCurrentPosition.latitude],
-      });
-    } catch (error) {}
-  }
-  async function loadInitialState() {
-    const { latitude, longitude } = await getCurrentLocation();
-    setInitialState({ latitude, longitude, zoom: 14 });
-    setPermissionGranted(true);
-  }
-  async function watchUserPostionChanges() {
-    const watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        setUserCurrentPosition({ latitude, longitude });
-        console.log({ position });
-        setUpdatedTimes(updatedTimes + 1);
-      },
-      (error) => console.error(error)
-    );
 
-    return () => navigator.geolocation.clearWatch(watchId);
+  async function loadInitialState() {
+    try {
+      const { latitude, longitude } = await getCurrentLocation();
+      setInitialState({ latitude, longitude, zoom: 14 });
+      setPermissionGranted(true);
+    } catch (error) {
+      console.error("Error getting initial location:", error);
+    }
+  }
+
+  function startTrackingUserPosition() {
+    if (!showUserLocationMarker) {
+      setShowUserLocationMarker(true);
+      navigator.geolocation.watchPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserCurrentPosition({ latitude, longitude });
+          setUpdatedTimes((prev) => prev + 1);
+        },
+        (error) => {
+          console.error("Error watching position:", error);
+        }
+      );
+    }
+  }
+
+  async function flyToUserCurrentPosition() {
+    const { latitude, longitude } = await getCurrentLocation();
+    mainMap?.flyTo({ center: [longitude, latitude] });
   }
 
   return (
@@ -99,13 +83,14 @@ export default function Home() {
       ) : (
         <h1>You must give permission to get your location </h1>
       )}
-      <div
-        className="absolute right-3 bottom-28"
-        onClick={flyToUserCurrentPosition}
-      >
+      <div className="absolute right-3 bottom-28">
         <FontAwesomeIcon
           icon={faLocationCrosshairs}
-          className="text-5xl text-primary"
+          className="text-5xl text-primary cursor-pointer"
+          onClick={() => {
+            flyToUserCurrentPosition();
+            startTrackingUserPosition();
+          }}
         />
       </div>
     </div>
