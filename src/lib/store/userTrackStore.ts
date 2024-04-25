04eb5787c28bf7d18d93a34a2de84fb5acf8b01f
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-04-25 21:21:57 +0900

    saving path on local storage

--- src/lib/store/userTrackStore.ts
+++ src/lib/store/userTrackStore.ts
@@ -1,16 +1,24 @@
 import { create } from "zustand";
 
 export const useUserTrackStore = create((set) => ({
-  userCurrentTrack: [],
+  userCurrentTrack: localStorage.getItem("track")
+    ? JSON.parse(localStorage.getItem("track"))
+    : [],
   isTrackingPosition: false,
   toggleTrackingPosition: () =>
     set((state) => ({
       isTrackingPosition: !state.isTrackingPosition,
     })),
   addCoordinateToCurrentTrack: ([longitude, latitude]) =>
-    set((state) => ({
-      userCurrentTrack: [...state.userCurrentTrack, [longitude, latitude]],
-    })),
+    set((state) => {
+      localStorage.setItem(
+        "track",
+        JSON.stringify([...state.userCurrentTrack, [longitude, latitude]])
+      );
+      return {
+        userCurrentTrack: [...state.userCurrentTrack, [longitude, latitude]],
+      };
+    }),
   userCurrentPosition: { latitude: 0, longitude: 0 },
   setUserCurrentPosition: ({ longitude, latitude }) => {
     console.log("TRY TO SET USER CURRENT POSITIION", { longitude, latitude });
