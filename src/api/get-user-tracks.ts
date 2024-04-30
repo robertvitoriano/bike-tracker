4a4ef4a3a120a30a29a3f3132fcaf0eb92c7866c
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-05-01 00:32:16 +0900

    add route to get user tracks

---
+++ src/api/get-user-tracks.ts
@@ -0,0 +1,13 @@
+import { api } from "@/lib/axios";
+export interface GetUserTracksResponse {
+  title: string;
+  _id: string;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+  coordinates: [[number, number]];
+}
+export async function getUserTracks() {
+  const response = await api.get<GetUserTracksResponse>("/tracks");
+
+  return response.data;
+}
---
+++ src/api/sign-in.ts
@@ -0,0 +1,9 @@
+import { api } from "@/lib/axios";
+
+export interface SignInBody {
+  email: string;
+  password: string;
+}
+export async function signIn({ email, password }: SignInBody) {
+  await api.post("/login", { email, password });
+}
