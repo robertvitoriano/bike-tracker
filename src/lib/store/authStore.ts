ee55df26cf54ed1af422d7dba5e8295ef4f73c57
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-05-03 13:14:10 +0900

    persisting token

--- src/lib/store/authStore.ts
+++ src/lib/store/authStore.ts
@@ -1,19 +1,29 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 interface IAuthStore {
   loggedUser: { name: string; email: string; username: string };
   token: string;
   setLoggedUser: Function;
   setToken: Function;
 }
-export const useAuthStore = create<IAuthStore>((set) => ({
-  loggedUser: null,
-  token: "",
-  setLoggedUser: (loggedUser) =>
-    set(() => ({
-      loggedUser,
-    })),
-  setToken: (token) =>
-    set(() => ({
-      token,
-    })),
-}));
+export const useAuthStore = create<IAuthStore>(
+  //@ts-ignore
+  persist(
+    (set) => ({
+      loggedUser: null,
+      token: "",
+      setLoggedUser: (loggedUser) =>
+        set(() => ({
+          loggedUser,
+        })),
+      setToken: (token) =>
+        set(() => ({
+          token,
+        })),
+    }),
+    {
+      name: "auth-store",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
