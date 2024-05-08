05a419b953bb6d78bf5b6408e0b1e29e0f5672d8
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-05-08 11:56:33 +0900

    add map provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/react-query";
 import { SaveTrackDialog } from "./components/SaveTrackDialog";
 import { PauseTrackingDialog } from "./components/PauseTrackingDialog";
+import { MapProvider } from "react-map-gl";
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <SaveTrackDialog />
-      <PauseTrackingDialog />
-    </QueryClientProvider>
+    <MapProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <SaveTrackDialog />
+        <PauseTrackingDialog />
+      </QueryClientProvider>
+    </MapProvider>
   );
 }
