143de94ea36c2d0e4a1ece7e069838dc34ce0050
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-05-08 10:44:53 +0900

    add mock strava path

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,14 +2,20 @@ import { getUserStatistics } from "@/api/get-user-statistics";
 import { getFormattedTime } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import { Skeleton } from "@/components/ui/skeleton";
-
+import { format } from "date-fns";
+import { getUserTracks } from "@/api/get-user-tracks";
+import stravaMockPath from "../../assets/strava-mock-path.jpg";
 export function Home() {
   const { data: statistics, isLoading } = useQuery({
     queryKey: ["get-user-statistics"],
     queryFn: getUserStatistics,
   });
+  const { data: userSavedTracks, isLoadingUserSavedTracks } = useQuery({
+    queryKey: ["get-user-tracks"],
+    queryFn: getUserTracks,
+  });
   return (
-    <div className="h-screen bg-muted">
+    <div className="bg-muted flex flex-col gap-4">
       <div className="flex flex-col gap-4 py-4 bg-secondary">
         <div className="flex flex-col gap-4 px-4">
           <div>Your performance:</div>
@@ -46,6 +52,51 @@ export function Home() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="p-4 bg-secondary flex flex-col gap-4">
+        {isLoadingUserSavedTracks && <Skeleton className="h-10" />}
+        {userSavedTracks && userSavedTracks.length === 0 ? (
+          <h1>You don't have any track</h1>
+        ) : (
+          userSavedTracks &&
+          userSavedTracks.map((track) => (
+            <div key={track._id} className="flex flex-col items-center">
+              <div className="w-full">
+                <h1 className="text-center text-4xl font-bold">
+                  {!isLoadingUserSavedTracks ? (
+                    track.title
+                  ) : (
+                    <Skeleton className="h-4 w-full" />
+                  )}
+                </h1>
+              </div>
+              <div className="flex justify-around">
+                <div className="p-4 bg-white">
+                  <span className="text-xl">
+                    {!isLoadingUserSavedTracks ? (
+                      format(new Date(track.createdAt), "dd/MM/yyyy")
+                    ) : (
+                      <Skeleton className="h-4 w-full" />
+                    )}
+                  </span>
+                </div>
+                <div className="p-4 bg-white">
+                  <span className="text-xl">
+                    {getFormattedTime(track.time)}
+                  </span>
+                </div>
+                <div className="p-4 bg-white">
+                  {!isLoadingUserSavedTracks ? (
+                    <span className="text-xl">{track.distance} Km</span>
+                  ) : (
+                    <Skeleton className="h-4 w-full" />
+                  )}
+                </div>
+              </div>
+              <img src={stravaMockPath} className="h-80 w-80" />
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
