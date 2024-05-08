df062f783f9712374ca638404d34d8fd9a67d9b0
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-05-08 09:44:57 +0900

    enable loading

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import { getUserStatistics } from "@/api/get-user-statistics";
 import { getFormattedTime } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "@/components/ui/skeleton";
+
 export function Home() {
   const { data: statistics, isLoading } = useQuery({
     queryKey: ["get-user-statistics"],
@@ -9,31 +11,41 @@ export function Home() {
   return (
     <div className="h-screen bg-muted">
       <div className="flex flex-col gap-4 py-4 bg-secondary">
-        {!isLoading && (
-          <div className="flex flex-col gap-4 px-4">
-            <div>Your performance:</div>
-            <div className="flex gap-4 ">
-              <div className="flex flex-col gap-2">
-                <span className="text-xs">Tracks</span>
-                <span className=" text-2xl font-bold">
-                  {statistics?.totalOfTracks || 0}
-                </span>
-              </div>
-              <div className="flex flex-col gap-2">
-                <span className="text-xs">Time</span>
-                <span className=" text-2xl font-bold">
-                  {getFormattedTime(statistics?.totalTime)}
-                </span>
-              </div>
-              <div className="flex flex-col gap-2">
-                <span className="text-xs">Distance</span>
-                <span className=" text-2xl font-bold">
-                  {statistics?.totalDistance.toFixed(2) || 0} Km
-                </span>
-              </div>
+        <div className="flex flex-col gap-4 px-4">
+          <div>Your performance:</div>
+          <div className="flex gap-4 ">
+            <div className="flex flex-col gap-2">
+              <span className="text-xs">Tracks</span>
+              <span className=" text-2xl font-bold">
+                {!isLoading ? (
+                  statistics?.totalOfTracks || 0
+                ) : (
+                  <Skeleton className="h-4 w-full" />
+                )}
+              </span>
+            </div>
+            <div className="flex flex-col gap-2">
+              <span className="text-xs">Time</span>
+              <span className=" text-2xl font-bold">
+                {!isLoading ? (
+                  getFormattedTime(statistics?.totalTime) || 0
+                ) : (
+                  <Skeleton className="h-4 w-full" />
+                )}
+              </span>
+            </div>
+            <div className="flex flex-col gap-2">
+              <span className="text-xs">Distance</span>
+              <span className=" text-2xl font-bold">
+                {!isLoading ? (
+                  `${statistics?.totalDistance.toFixed(2) || 0} Km`
+                ) : (
+                  <Skeleton className="h-4 w-full" />
+                )}
+              </span>
             </div>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
