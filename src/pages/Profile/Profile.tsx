e476b62f8a151092ccc99bf47da4c8dba6904103
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-06-16 16:49:53 +0900

    add logout button

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,12 +1,26 @@
 import { useAuthStore } from "@/lib/store/authStore";
-
+import { useNavigate } from "react-router-dom";
 export function Profile() {
   const loggedUser = useAuthStore((state: any) => state.loggedUser);
+  const setLoggedUser = useAuthStore((state: any) => state.setLoggedUser);
+  const setToken = useAuthStore((state: any) => state.setToken);
+  const navigate = useNavigate();
 
+  function handleLoggout() {
+    setLoggedUser(null);
+    setToken("");
+    navigate("/");
+  }
   return (
-    <div className="flex flex-col w-screen h-screen items-center justify-center relative bg-primary">
-      <h1>{loggedUser.name}</h1>
-      <img src={loggedUser.avatar} />
+    <div className="flex flex-col w-screen h-screen items-center gap-10 relative bg-primary pt-8">
+      <h1 className="text-2xl">{loggedUser.name}</h1>
+      <img src={loggedUser.avatar} className="h-44 w-44 rounded-full" />
+      <div
+        className="text-black bg-secondary hover:text-white hover:bg-black cursor-pointer py-4 px-6 rounded-full"
+        onClick={handleLoggout}
+      >
+        <span className="text-2xl">LOGOUT</span>
+      </div>
     </div>
   );
 }
